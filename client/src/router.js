44c415bb94ca58998b11efc5b289dd7aa1b8ca15
shirow ozawa <striderkein@users.noreply.github.com>
2021-02-07 20:45:59 +0900

    refactor:deleted List.vue
    vue-good-tableの利用により汎用リストコンポーネントの定義は不要となったため。

--- client/src/router.js
+++ client/src/router.js
@@ -1,16 +1,11 @@
 import Vue from "vue";
 import Router from "vue-router";
-import List from "./components/List.vue";
 import LoginCheck from "./components/LoginCheck.vue";
 
 Vue.use(Router);
 export default new Router({
   mode: "history",
   routes: [
-      {
-        path: '/list',
-        component: List
-      },
       {
         path: '/login_check',
         component: LoginCheck
